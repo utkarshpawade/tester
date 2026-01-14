35a0ac89e34964759a5a3aeefc7f3debf21ce59e
Utkarsh <utkarshpawadecse@gmail.com>
2026-01-14 05:58:57 +0530

    UHBNK

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import {useState, useEffect} from "react";
-function App() {
+import { useState } from "react";
+const App=()=>{
+    return <div>
+      <Lightbulb/>
+    </div>
+}
+const Lightbulb=()=>{
+  const[Bulbon, Setbulb]=useState(true);
   return <div>
-    hi 
+    <Bulbstate Bulbon={Bulbon}/>
+    <Togglebulbstate Bulbon={Bulbon} Setbulb={Setbulb}/>
   </div>
 }
-
-
+const Bulbstate=({Bulbon})=>{
+  return <div>
+    {Bulbon ? "bulb chalu": "bulbband"}
+  </div>
+}
+const Togglebulbstate=({Bulbon,  Setbulb})=>{
+  const toggle=()=>{
+    Setbulb(currst=>!currst)
+  }
+  return <div>
+    <button onClick={toggle}>toggle it</button>
+  </div>
+}
 export default App
