fc3aca2281a6a8ed54e4cf32d3f56418728e9e9c
Utkarsh <utkarshpawadecse@gmail.com>
2026-01-18 19:34:47 +0530

    hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,11 @@
-import { useState, createContext, useContext } from "react";
-const Bulbcontext=createContext();
-
-const contextprovider=({children})=>{
-const[Bulbon, Setbulb]=useState(true);
-return 
-  <Bulbcontext.Provider value={
-  {
-        Bulbon: Bulbon,
-        Setbulb: Setbulb
-  }
-}>
-{children}
-</Bulbcontext.Provider>
-
-
-}
+import { useState} from "react";
 const App=()=>{
     
     return <div>
-      <contextprovider>
-        <Lightbulb/>
-      </contextprovider>
-      
+
     </div>
 }
 
-const Lightbulb=()=>{
 
-  return <div>
-    <Bulbstate/>
-    <Togglebulbstate />
-  </div>
-}
-
-const Bulbstate=()=>{
-  const {Bulbon}=useContext(Bulbcontext);
-  return <div>
-    {Bulbon ? <img src='https://www.w3schools.com/js/pic_bulboff.gif'></img>: <img src="https://www.w3schools.com/js/pic_bulbon.gif"></img>}
-  </div>
-}
-
-const Togglebulbstate=()=>{
-  const {Bulbon, Setbulb}=useContext(Bulbcontext);
-  const toggle=()=>{
-    Setbulb(currst=>!currst)
-  }
-  return <div>
-    <button onClick={toggle}>toggle it</button>
-  </div>
-}
 
 export default App
