af88dcb93bbc8ce7dabf328c9f1103c0c258e9e2
Utkarsh <utkarshpawadecse@gmail.com>
2026-01-23 21:38:46 +0530

    chec

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,10 @@ import { useEffect, useState} from "react";
 import { useFetch } from "./useFetch";
 
 const App=()=>{
-    const {finalData, loading}=useFetch("https://jsonplaceholder.typicode.com/posts/2");
-    if(loading){
-        return <div>
-            loading...
-        </div>
-    }
+    
     return (
     <div>
-        {finalData.title}
+
     </div>
     )
 }
---
+++ src/usePrev.js
@@ -0,0 +1,3 @@
+export const usePrev=()=>{
+    const ref=useRef();
+}
