af230a7050c03955dc1e942a0e3ce40338def138
Utkarsh <utkarshpawadecse@gmail.com>
2026-01-24 08:57:50 +0530

    debounce logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import { useEffect, useState} from "react";
 import { useFetch } from "./useFetch";
+import { send } from "vite";
+
+function useDebounce(ogfun){
+    const currentClock=useRef();
+    const fn=()=>{
+        clearTimeout(currentClock.current);
+        currentClock.current=setTimeout()
+    }
+}
 
 const App=()=>{
-    
+    function sendDatatoBackend(){
+        fetch("");
+    }
+    const debouncedfn=useDebounce(sendDatatoBackend);
     return (
     <div>
-
+        <input type="text" onChange={debouncedfn}></input>
     </div>
     )
 }
